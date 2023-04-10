f237d027b3f5461fa0543cb690b7bf0846af6028
david <isebarn182@gmail.com>
2023-04-10 13:46:54 +0100

    make sure its long enough

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from io import StringIO
 
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def open_ai(
@@ -43,24 +43,30 @@ def open_ai(
     return response.choices[0].text.strip()
 
 def create_prompt(form):
-    prompt = "Make me a meal plan based on the following. Height: {} , Weight: {}, Age: {}, Target Weight: {} ".format(form.height, form.weight, form.age, form.target_weight)
-    prompt += "Make a weekly meal plan where each day has breakfast, snack, lunch, afternoon_snack and dinner. Return as a csv with a | separator instead of a comma."
-    prompt += "Example row looks like this: Monday|eggs with tomato|apple|chicken salad|banana|steak and potatoes"
-    
-    response = open_ai(prompt)
-    csv = response
-    #csv = 'Monday|Oatmeal with banana and walnuts|Yogurt with granola|Turkey sandwich with lettuce and tomato|Carrot sticks and hummus|Grilled salmon with roasted vegetables|\n\nTuesday|Scrambled eggs with spinach and feta cheese|Berries and almonds|Salad with grilled chicken|Apple slices with peanut butter|Stir fry with tofu and vegetables|\n\nWednesday|Whole wheat toast with peanut butter and banana|String cheese and crackers|Veggie wrap with hummus|Celery sticks with cottage cheese|Grilled steak with roasted potatoes and broccoli|\n\nThursday|Smoothie with yogurt, banana, and berries|Trail mix with dried fruit and nuts|Grilled turkey burger on a whole wheat bun|Cucumber slices with Greek yogurt dip|Vegetable stir fry over brown rice|\n\nFriday|Oatmeal pancakes with fresh fruit|Yogurt parfait with granola and berries|Turkey wrap with lettuce, tomato, and avocado|Apple slices with almond butter|Baked salmon with quinoa and roasted vegetables|\n\nSaturday|Eggs Benedict on whole wheat toast|Cheese cubes and crackers|Greek salad with grilled chicken breast|Carrot sticks and hummus dip|Grilled shrimp with brown rice and steamed vegetables |\n\nSunday|Blueberry muffins and Greek yogurt |Trail mix with dried fruit and nuts |Veggie wrap with hummus |Celery sticks with cottage cheese |Chicken stir fry over brown rice'
-
-    samba = reader(StringIO(csv), delimiter="|")
-
     meals = []
-    for row in samba:
-        if not any(row): continue
-        data = {key: value for key, value in zip(["breakfast", "snack1", "lunch", "snack2", "dinner"], row)}
 
-        meals.append(data)
+    counter = 0
+    while len(meals) < 7 and counter < 10:
+        
+        counter += 1
+        prompt = "Make me a meal plan based on the following. Height: {} , Weight: {}, Age: {}, Target Weight: {} ".format(form.height, form.weight, form.age, form.target_weight)
+        prompt += "Make a weekly meal plan where each day has breakfast, snack, lunch, afternoon_snack and dinner. Return as a csv with a | separator instead of a comma."
+        prompt += "Example row looks like this: Monday|eggs with tomato|apple|chicken salad|banana|steak and potatoes"
+        
+        response = open_ai(prompt)
+        csv = response
+        #csv = 'Monday|Oatmeal with banana and walnuts|Yogurt with granola|Turkey sandwich with lettuce and tomato|Carrot sticks and hummus|Grilled salmon with roasted vegetables|\n\nTuesday|Scrambled eggs with spinach and feta cheese|Berries and almonds|Salad with grilled chicken|Apple slices with peanut butter|Stir fry with tofu and vegetables|\n\nWednesday|Whole wheat toast with peanut butter and banana|String cheese and crackers|Veggie wrap with hummus|Celery sticks with cottage cheese|Grilled steak with roasted potatoes and broccoli|\n\nThursday|Smoothie with yogurt, banana, and berries|Trail mix with dried fruit and nuts|Grilled turkey burger on a whole wheat bun|Cucumber slices with Greek yogurt dip|Vegetable stir fry over brown rice|\n\nFriday|Oatmeal pancakes with fresh fruit|Yogurt parfait with granola and berries|Turkey wrap with lettuce, tomato, and avocado|Apple slices with almond butter|Baked salmon with quinoa and roasted vegetables|\n\nSaturday|Eggs Benedict on whole wheat toast|Cheese cubes and crackers|Greek salad with grilled chicken breast|Carrot sticks and hummus dip|Grilled shrimp with brown rice and steamed vegetables |\n\nSunday|Blueberry muffins and Greek yogurt |Trail mix with dried fruit and nuts |Veggie wrap with hummus |Celery sticks with cottage cheese |Chicken stir fry over brown rice'
 
-    return meals
+        samba = reader(StringIO(csv), delimiter="|")
+
+        for row in samba:
+            if not any(row): continue
+            data = {key: value for key, value in zip(["breakfast", "snack1", "lunch", "snack2", "dinner"], row)}
+
+            meals.append(data)
+
+
+    return meals[0:7]
 
 
 
